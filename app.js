0d421068465c109027dbb3e2dc483f1461c0ab56
MichaelD <enee.kors@mail.ru>
2021-10-03 12:24:39 +0300

    add cheat

--- app.js
+++ app.js
@@ -91,3 +91,14 @@ function getRandomColor() {
     const index = Math.floor(Math.random() * colors.length)
     return colors[index]
 }
+
+function winTheGame() {
+    function kill() {
+        const circle = document.querySelector('.circle')
+
+            if (circle) {
+                circle.click()
+            }
+    }
+    setInterval(kill, 42)
+}
